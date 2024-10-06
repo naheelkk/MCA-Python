7d58d1b003488049dc2c45e6b891c46b7fa0c71d
Naheel <96335640+naheelkk@users.noreply.github.com>
2024-10-06 19:09:54 +0530

    Update Prg8-Leap_year.py

--- October/04-10-2024/Prg8-Leap_year.py
+++ October/04-10-2024/Prg8-Leap_year.py
@@ -1,8 +1,8 @@
 current_year = 2024
-end_year = int(input("Complete the range of years From 2024 to ?"))
+end_year = int(input(f'Complete the range of years From {current_year} to ?'))
 years_in_between = []
 leap_years = []
-for year in range(2024, end_year + 1):
+for year in range(current_year, end_year + 1):
     if(year % 4 == 0 and (year % 400 == 0 or year % 100 != 0)):
         leap_years.append(year)
 
