cc9eb1cdb211da4c86c071b5ddfdedaea39b8153
Naheel <96335640+naheelkk@users.noreply.github.com>
2024-10-18 20:12:08 +0530

    Delete October/18-10-24/occurance-of-word.py

--- October/18-10-24/occurance-of-word.py
+++
@@ -1,8 +0,0 @@
-input = str(input("Enter a line of text"))
-splitted = input.split(' ')
-counted = []
-for i in splitted:
-    if i not in counted:
-        count = splitted.count(i)
-        print(f'Count of {i} is {count}')
-        counted.append(i)
