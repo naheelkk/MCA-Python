b276a7191b47586439b323d7eb3551e2d66206fd
Naheel <96335640+naheelkk@users.noreply.github.com>
2024-10-23 06:30:54 +0100

    Create prgrm25-builtin-fun.py

---
+++ October/23-10-2024/prgrm25-builtin-fun.py
@@ -0,0 +1,11 @@
+lst = [1,5,342,2345,5743,12342,45]
+minimum_value = min(lst)
+max_value = max(lst)
+# count_of_list = lst.count()
+length_of_lsit = len(lst)
+reversed = lst.reverse()
+
+print(f'Minimum Value of the list is {minimum_value}')
+print(f'Maximum Value of the list is {max_value}')
+print(f'Length of the list is {length_of_lsit}')
+print(f'Minimum Value of the list is {minimum_value}')
